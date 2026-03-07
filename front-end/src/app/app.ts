e690ef0ac3a95b55682c13a05fb0ebfe7966ab0d
Kiran Anand <liteninkiran@hotmail.com>
2026-03-07 19:53:53 +0000

    Add drag-and-drop

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,12 +1,60 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss'],
 })
 export class App {
-  protected readonly title = signal('front-end');
+  files: File[] = [];
+  uploading = false;
+
+  constructor(private http: HttpClient) {}
+
+  onFileDropped(dt: DataTransfer | null) {
+    if (dt?.files) {
+      this.addFiles(dt.files);
+    }
+  }
+
+  fileBrowseHandler(files: FileList | null) {
+    if (files) {
+      this.addFiles(files);
+    }
+  }
+
+  private addFiles(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      if (files[i].type === 'text/csv') {
+        this.files.push(files[i]);
+      } else {
+        alert('Only CSV files are allowed!');
+      }
+    }
+  }
+
+  uploadFiles() {
+    if (!this.files.length) return;
+
+    const formData = new FormData();
+    this.files.forEach((file) => formData.append('files[]', file, file.name));
+
+    this.uploading = true;
+    this.http.post('https://your-backend-endpoint.com/upload', formData).subscribe({
+      next: (res) => {
+        console.log('Upload successful', res);
+        this.files = [];
+        this.uploading = false;
+      },
+      error: (err) => {
+        console.error('Upload failed', err);
+        this.uploading = false;
+      },
+    });
+  }
 }
