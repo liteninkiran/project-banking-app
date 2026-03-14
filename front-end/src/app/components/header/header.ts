37b22b4e5ae4cc7b7cac02c3e84eb4ac0488a215
Kiran Anand <liteninkiran@hotmail.com>
2026-03-14 20:27:11 +0000

    Use signals

--- front-end/src/app/components/header/header.ts
+++ front-end/src/app/components/header/header.ts
@@ -1,5 +1,6 @@
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RuxButton, RuxClock, RuxGlobalStatusBar, RuxIcon } from '@astrouxds/angular';
 import { map, timer } from 'rxjs';
 
@@ -15,12 +16,18 @@ const darkTheme = 'dark-theme';
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [RuxButton, RuxIcon, RuxGlobalStatusBar, RuxClock, DatePipe, AsyncPipe],
+  imports: [RuxButton, RuxIcon, RuxGlobalStatusBar, RuxClock, DatePipe],
 })
 export class Header {
-  darkMode = true;
-  icon = nightIcon;
-  time$ = timer(this.getInitialDelay(), 60000).pipe(map(() => new Date()));
+  private readonly intervalSeconds = 10;
+  private time$ = timer(
+    this.getInitialDelay(this.intervalSeconds),
+    this.intervalSeconds * 1000,
+  ).pipe(map(() => new Date()));
+
+  public currentTime = toSignal(this.time$, { initialValue: new Date() });
+  public darkMode = true;
+  public icon = nightIcon;
 
   constructor() {
     if (localStorage.getItem('theme') === lightTheme) {
@@ -45,8 +52,17 @@ export class Header {
     document.body.classList.toggle(lightTheme, !this.darkMode);
   }
 
-  private getInitialDelay() {
+  private getInitialDelay(n: number): number {
     const now = new Date();
-    return (60 - now.getSeconds()) * (1000 - now.getMilliseconds());
+    const seconds = now.getSeconds();
+    const ms = now.getMilliseconds();
+
+    // Find next multiple of n
+    const next = Math.ceil((seconds + ms / 1000) / n) * n;
+
+    // It takes about 200ms to do all of this
+    const compOffset = 200;
+
+    return next * 1000 - (seconds * 1000 + ms) - compOffset;
   }
 }
