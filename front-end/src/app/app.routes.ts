1ff8d077517f0e4f45ecf01455ee713266c13de7
Kiran Anand <liteninkiran@hotmail.com>
2026-04-05 18:55:29 +0100

    Remove FE code

--- front-end/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { UploadData } from './pages/upload-data/upload-data';
-
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'upload', component: UploadData },
-];
